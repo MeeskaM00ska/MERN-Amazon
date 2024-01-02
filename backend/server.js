0fcfe704c9557b7ead025ef118ead97a66a7c4df
Mingyang Li <nathannnnn.li@mail.utoronto.ca>
2024-01-02 15:57:07 +0800

    backend-auth

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   return res.send("this is the first page");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 //under all routes
 app.use(notFound);
