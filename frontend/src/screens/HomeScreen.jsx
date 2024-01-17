cedf678c57d8a3d99e24ce598c245f7f659cc5c5
Mingyang Li <nathannnnn.li@mail.utoronto.ca>
2024-01-17 11:38:10 +0800

    page

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,10 +1,14 @@
 import { Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import Paginate from "../components/Paginate";
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
@@ -18,7 +22,7 @@ const HomeScreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => {
+            {data.products.map((product) => {
               return (
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                   <Product product={product} />
@@ -26,6 +30,11 @@ const HomeScreen = () => {
               );
             })}
           </Row>
+          <Paginate
+            pages={data.pages}
+            page={data.page}
+            // keyword={keyword ? keyword : ''}
+          />
         </>
       )}
     </>
